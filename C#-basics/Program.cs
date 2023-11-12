cb40e2005560d03a71119c3c022de6dd7f735759
Hendy <hendy.sg02@gmail.com>
2023-11-12 23:53:41 +0700

    Basic Console WriteLine & ReadLine

---
+++ C#-basics/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace C__basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     /|");
+            Console.WriteLine("    / |");
+            Console.WriteLine("   /  |");
+            Console.WriteLine("  /   |");
+            Console.WriteLine(" /____|");
+
+            // Console.ReadLine();
+        }
+    }
+}
